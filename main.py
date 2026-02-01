d3eee0534678758b1572de3a59dfd52ebee9bd12
Saikodali7095 <pavansaikrishna916@gmail.com>
2026-02-01 09:53:12 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+def main():
+    print("Project: StudyBuddy")
+    print("This project will help students track tasks and deadlines.")
+    print("Current version: setup + runnable script confirmed!")
+
+
+if __name__ == "__main__":
+    main()
